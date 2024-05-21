ce201f52191f96b57fe4f6149afd0b21b38e096e
lou-pastorino <168196916+lou-pastorino@users.noreply.github.com>
2024-05-21 10:47:54 +0100

    Added Fade in effects to each section

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      console.log("true");
+      entry.target.classList.add("show");
+    } else {
+      console.log("false)");
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
